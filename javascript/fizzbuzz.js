aa267023226f006a38c4d16d246fe19fd59893b9
P Campbell <pcampbell.edu@gmail.com>
2021-10-28 20:24:16 -0400

    Update fizzbuzz.js

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -1,41 +1,38 @@
-"use strict";
+'use strict'
 /*
 * implement fizzbuzz challenge
 * 
 * add a header + ul to document body
 * loop through and add a <li> for 
 * the fizz buzz logic
-*
+* 2021-10-27 update syntax, remove var
+* 
 * @author P Campbell
 *
 */
-var fizzbuzz = function ( ) {
+function fizzbuzz( ) {
   console.log ("Dom loaded")
-  var h1 = document.createElement('h1');
+  let h1 = document.createElement('h1');
   document.body.appendChild(h1)
-  h1.innerHTML = "Welcome to FizzBuzz"
+  h1.textContent = "Welcome to FizzBuzz"
   
-  var ul = document.createElement('ul');
-  var docUl = document.body.appendChild(ul)
-  var li; 
+  let ul = document.createElement('ul');
+  document.body.appendChild(ul)
+  let li; 
  
-  for (var i=1; i <= 100; i++)  {
+  for (let i=1; i <= 100; i++)  {
 	li = document.createElement('li');
-    if (i%3 == 0 ) {
-		// should be innerText???
-		li.innerText = "Fizz";
+    	if (i%3 == 0 ) {
+		li.textContent  = "Fizz";
 	}  
     if (i%5 == 0 ) {
-		li.innerText += "Buzz";
+		li.textContent += "Buzz";
     }
 	if (i%5 != 0 && i%3 != 0)
-		li.innerText = i;
-	docUl.appendChild(li)
-	console.log(li.innerText + " " + i)
+		li.textContent = i;
+	ul.appendChild(li)
+	console.log(li.textContent + " " + i)
 	}
-	// find out how to close a tag  /ul not valid
-	// ul = document.createElement('/ul');
-    //docUl.appendChild(ul)
 }
 console.log ("before fizzbuzz")
 document.addEventListener("DOMContentLoaded", fizzbuzz);
